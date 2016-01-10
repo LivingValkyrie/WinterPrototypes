55491ae1df22652e68561d5cd3cd485fd048d54f
LivingValkyrie <deadwynn@gmail.com>
2016-01-10 16:47:24 -0600

    added gameManager
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

--- 2d Roguelike training/Assets/_Scripts/GameManager.cs
+++ 2d Roguelike training/Assets/_Scripts/GameManager.cs
@@ -8,13 +8,32 @@ using System.Collections.Generic;
 /// 
 /// Description: GameManager 
 /// </summary>
+[RequireComponent(typeof (BoardManager))]
 public class GameManager : MonoBehaviour {
     #region Fields
 
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    int level = 3;
+
     #endregion
 
-    void Start() { }
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }else if (instance != null) {
+            Destroy(gameObject);
+        }
 
-    void Update() { }
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
 
+    void InitGame() {
+        boardScript.SetupScene(level);
+    }
+
+    void Update() {}
 }
---
+++ 2d Roguelike training/Assets/_Scripts/Loader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: Loader 
+/// </summary>
+public class Loader : MonoBehaviour {
+    #region Fields
+
+    public GameObject gameManager;
+
+    #endregion
+
+    void Awake() {
+        if (GameManager.instance == null) {
+            Instantiate(gameManager);
+        }
+    }
+}
