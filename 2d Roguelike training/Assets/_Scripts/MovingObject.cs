8da188b3aac6257cd505646c69a88cb44d613b54
LivingValkyrie <deadwynn@gmail.com>
2016-01-10 17:12:18 -0600

    added MovingObject
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ 2d Roguelike training/Assets/_Scripts/MovingObject.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: Movin 
+/// </summary>
+public abstract class MovingObject : MonoBehaviour {
+    #region Fields
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+    BoxCollider2D boxCollider;
+    Rigidbody2D rb2D;
+    float inverseMoveTime;
+
+    #endregion
+
+    protected virtual void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+    }
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit) {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+
+        if (hit.transform == null) {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+
+        return false;
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end) {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (sqrRemainingDistance > float.Epsilon) {
+            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected virtual void AttemptMove<TComponent>(int xDir, int yDir) where TComponent : Component {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null) {
+            return;
+        }
+
+        TComponent hitComponent = hit.transform.GetComponent<TComponent>();
+
+        if (!canMove && hitComponent != null) {
+            OnCantMove(hitComponent);
+        }
+    }
+
+    protected abstract void OnCantMove<TComponent>(TComponent component) where TComponent : Component;
+
+}
