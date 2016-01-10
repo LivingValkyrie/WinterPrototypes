a0f680aa6536835731eab8b3835ffdf9cbd9c14a
LivingValkyrie <deadwynn@gmail.com>
2016-01-10 16:36:24 -0600

    finished tile assets. did board manager script and updated meta files
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ 2d Roguelike training/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: GameManager 
+/// </summary>
+public class GameManager : MonoBehaviour {
+    #region Fields
+
+    #endregion
+
+    void Start() { }
+
+    void Update() { }
+
+}
